4e0d3f3e6e05b6e1811103be7b72f6ca8d4dacb1
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-21 11:22:41 -0500

    db connection msg

---
+++ modules/connection.js
@@ -0,0 +1,13 @@
+const sequelize = require('./db');
+const School = require('./school');
+
+(async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log('Database & tables created!');
+  } catch (error) {
+    console.error('Error syncing database:', error);
+  } finally {
+    process.exit();
+  }
+})();
