196bb7fbbf533b19f283086054863d66790ef3db
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-21 11:22:55 -0500

    db creation script

---
+++ modules/db.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+const env = require('dotenv');
+const path = require('path');
+env.config(path.join(__dirname , '../.env'));
+
+// -------------------------
+// Set up a sequelize object
+// -------------------------
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME, // Database name
+  process.env.DB_USER, // Database user
+  process.env.DB_PASSWORD, // Database password
+  {
+    host: process.env.DB_HOST, // Host
+    dialect: 'mysql', // Dialect
+    port: process.env.DB_PORT || 3306, // Port (default to 3306 for MySQL)
+    dialectOptions: {
+      // Remove SSL options, as they are unnecessary for local databases
+    },
+  }
+);
+
+// Test to check for the Data Base Connection
+sequelize
+.authenticate()
+.then(() => {
+    console.log('Connection has been established successfully.');
+})
+.catch((err) => {
+    console.error('Unable to connect to the database:', err);
+});
+
+module.exports = sequelize;
