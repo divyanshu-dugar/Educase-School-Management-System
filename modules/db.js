62d95849594517523eb9adab216b17e3c9225016
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-21 15:26:10 -0500

    using aiven now

--- modules/db.js
+++ modules/db.js
@@ -7,20 +7,53 @@ env.config(path.join(__dirname , '../.env'));
 // Set up a sequelize object
 // -------------------------
 
+// MYSQL - Local
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME, // Database name
+//   process.env.DB_USER, // Database user
+//   process.env.DB_PASSWORD, // Database password
+//   {
+//     host: process.env.DB_HOST, // Host
+//     dialect: 'mysql', // Dialect
+//     dialectModule: require('mysql2'),
+//     port: process.env.DB_PORT || 3306, // Port (default to 3306 for MySQL)
+//     dialectOptions: {
+//       // Remove SSL options, as they are unnecessary for local databases
+//     },
+//   }
+// );
+
+// GOOGLE CLOUD
+
+// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   dialect: 'mysql',
+//   host: process.env.DB_HOST, // This will be the connection name for Cloud SQL or public IP for local development
+//   dialectOptions: {
+//     socketPath: process.env.DB_HOST && process.env.DB_HOST.startsWith('/cloudsql') ? process.env.DB_HOST : undefined,
+//   },
+// });
+
+// Aiven
+
 const sequelize = new Sequelize(
-  process.env.DB_NAME, // Database name
-  process.env.DB_USER, // Database user
+  process.env.DB_NAME,   // Database name
+  process.env.DB_USER,   // Database user
   process.env.DB_PASSWORD, // Database password
   {
-    host: process.env.DB_HOST, // Host
-    dialect: 'mysql', // Dialect
-    dialectModule: require('mysql2'),
-    port: process.env.DB_PORT || 3306, // Port (default to 3306 for MySQL)
+    host: process.env.DB_HOST,    // Database host (Aiven's provided host)
+    port: process.env.DB_PORT,    // Database port
+    dialect: 'mysql',             // Database dialect (MySQL in this case)
     dialectOptions: {
-      // Remove SSL options, as they are unnecessary for local databases
+      ssl: {
+        require: true,             // Enforces SSL
+        rejectUnauthorized: false, // Bypasses self-signed certificate errors
+      },
     },
+    logging: false,   // Optional: Set to true to see SQL queries in console
   }
 );
+
 
 // Test to check for the Data Base Connection
 sequelize
