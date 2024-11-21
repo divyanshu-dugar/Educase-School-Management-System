66fa18f5ced635f9306e0ed1708f296234fb87d3
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-21 16:38:26 -0500

    removed direct host and added process.env.DB_HOST

--- modules/db.js
+++ modules/db.js
@@ -9,7 +9,7 @@ env.config(path.join(__dirname , '../.env'));
 
 // Aiven
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'school-management-divyu2004-2029.e.aivencloud.com',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
   dialectModule: require('mysql2'),
