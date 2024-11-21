cb386d7472c45ef97aa6a10ff5422a1b8fd7d808
Divyanshu Dugar <divyu2004@gmail.com>
2024-11-21 15:54:07 -0500

    FIXED DB name

--- modules/db.js
+++ modules/db.js
@@ -36,7 +36,7 @@ env.config(path.join(__dirname , '../.env'));
 
 // Aiven
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'mysql',
